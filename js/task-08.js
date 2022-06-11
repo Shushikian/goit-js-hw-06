5c3ec61a92106f7a025c2675f807d3908aba8ddc
Dmitry Shushaiev <shushikian@gmail.com>
2022-06-12 01:29:12 +0300

    end dz

---
+++ js/task-08.js
@@ -0,0 +1,72 @@
+const form = document.querySelector('.login-form');
+const subButton = document.querySelector('button');
+// const inputEmain = document.querySelector('input[type="email"]');
+// const inputPassword = document.querySelector('input[type="password"]')
+
+let user = {
+    email: '',
+    password: '',
+};
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email.value;
+  const password = formEl.password.value
+  console.log(email)
+  console.log(password)
+  if (email === "" || password === "") {
+    return alert('Please fill every field to continued!');
+  }
+    user.email = email;
+    user.password = password;
+  event.currentTarget.reset();
+  return user;
+  // console.dir(event.currentTarget.elements);
+  //   console.log(event.currentTarget.elements.email.value);
+}
+console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// subButton.addEventListener('click', (event) => {
+//     event.preventDefault();
+
+//   if (inputEmain === '' || inputPassword === '') {
+//     alert('Please fill every field to continued!');
+//     console.log(`inputEmain: fail`,inputEmain);
+//     console.log(`inputPassword: fail`,inputPassword);
+//   } else {
+//     user.email = inputEmain;
+//     user.password = inputPassword;
+//     console.log(`inputEmain: tru`,inputEmain);
+//     console.log(`inputPassword: tru`,inputPassword);
+//     console.log(`user:`,user);
+//         event.querySelector('.login-form').reset();
+//     return user;
+//   }
+// });
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const body = document.querySelector('body');
+const button = document.querySelector('button');
+const span = document.querySelector('p span.color');
+
+button.addEventListener('click', changeColor)
+
+function changeColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
